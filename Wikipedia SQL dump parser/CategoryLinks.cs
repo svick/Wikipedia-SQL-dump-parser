d7e538acee71ee3d49645ba0a50c551f4876da0a
Petr Onderka <gsvick@gmail.com>
2010-10-03 19:45:34 +0200

    added CategoryLinks

---
+++ Wikipedia SQL dump parser/CategoryLinks.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+
+namespace WpSqlDumpParser
+{
+	class CategoryLinks : Dump<CategoryLink>
+	{
+		CategoryLinks instance;
+		public CategoryLinks Instance
+		{
+			get
+			{
+				if (instance == null)
+					instance = new CategoryLinks();
+				return instance;
+			}
+		}
+
+		private CategoryLinks()
+		{ }
+
+		public override string Name
+		{
+			get { return "categorylinks"; }
+		}
+
+		public override IEnumerable<CategoryLink> Get(Stream stream)
+		{
+			Parser parser = new Parser();
+			return from values in parser.Parse(stream, 4)
+						 select new CategoryLink(
+							 values[0].ToInt32(),
+							 values[1].ToString(),
+							 values[2].ToString()) into categoryLink
+						 where categoryLink.FromId != 0
+						 select categoryLink;
+		}
+	}
+}
