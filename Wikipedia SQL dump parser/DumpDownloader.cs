3c9a044397710a432702c3be6c1e8981e3005c5f
Petr Onderka <gsvick@gmail.com>
2010-10-03 22:13:24 +0200

    implemented retrying downloading

--- Wikipedia SQL dump parser/DumpDownloader.cs
+++ Wikipedia SQL dump parser/DumpDownloader.cs
@@ -7,6 +7,8 @@ namespace WpSqlDumpParser
 {
 	class DumpDownloader
 	{
+		static readonly TimeSpan initialWaitTime = TimeSpan.FromSeconds(1);
+
 		public static Stream DownloadDump(string wiki, string dump, DateTime date)
 		{
 			string url = string.Format("http://download.wikimedia.org/{0}/{2}/{0}-{2}-{1}.sql.gz", wiki, dump, date.ToString("yyyyMMdd"));
@@ -14,7 +16,21 @@ namespace WpSqlDumpParser
 
 			WebClient wc = new WebClient();
 			wc.Headers[HttpRequestHeader.UserAgent] = "[[w:en:User:Svick]] SQL dump parser";
-			Stream httpStream = wc.OpenRead(url);
+			Stream httpStream = null;
+			int i = 0;
+			while (httpStream == null)
+				try
+				{
+					httpStream = wc.OpenRead(url);
+				}
+				catch (WebException ex)
+				{
+					TimeSpan waitTime = TimeSpan.FromSeconds(initialWaitTime.TotalSeconds * Math.Pow(2, i));
+					Console.Error.WriteLine(ex.Message);
+					Console.Error.WriteLine("Waiting {0} s.", (int)waitTime.TotalSeconds);
+					System.Threading.Thread.Sleep(waitTime);
+					i++;
+				}
 			GZipStream gzipStream = new GZipStream(httpStream, CompressionMode.Decompress);
 			return gzipStream;
 		}
