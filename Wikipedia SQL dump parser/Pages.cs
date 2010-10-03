47d682919fdb4d5a44d011bd826489ad0671c828
Petr Onderka <gsvick@gmail.com>
2010-10-03 19:10:38 +0200

    read Page values from parsed

---
+++ Wikipedia SQL dump parser/Pages.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections.Generic;
+using System.IO;
+
+namespace WpSqlDumpParser
+{
+	static class Pages
+	{
+		public IEnumerable<Page> Get(Stream stream)
+		{
+			Parser parser = new Parser();
+			foreach (var values in parser.Parse(stream, 12))
+				yield return new Page(
+					values[0].ToInt32(),
+					values[1].ToInt32(),
+					values[2].ToString(),
+					values[5].ToBoolean());
+		}
+	}
+}
