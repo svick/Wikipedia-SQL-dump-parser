fd32e9729a37ce303889b0ba03e8c8f4baf58198
Petr Onderka <gsvick@gmail.com>
2010-10-03 19:23:09 +0200

    added DumpDownloader

---
+++ Wikipedia SQL dump parser/DumpDownloader.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.IO;
+using System.IO.Compression;
+using System.Net;
+
+namespace WpSqlDumpParser
+{
+	class DumpDownloader
+	{
+		public static Stream DownloadDump(string wiki, string dump, DateTime date)
+		{
+			string url = string.Format("http://download.wikimedia.org/{0}/{2}/{0}-{2}-{1}.sql.gz", wiki, dump, date.ToString("yyyyMMdd"));
+
+			WebClient wc = new WebClient();
+			wc.Headers[HttpRequestHeader.UserAgent] = "[[w:en:User:Svick]] SQL dump parser";
+			Stream httpStream = wc.OpenRead(url);
+			GZipStream gzipStream = new GZipStream(httpStream, CompressionMode.Decompress);
+			return gzipStream;
+		}
+	}
+}
