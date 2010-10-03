98a1176f3786663d095456f1be3d5aaefb530a76
Petr Onderka <gsvick@gmail.com>
2010-10-03 20:07:05 +0200

    log downloading

--- Wikipedia SQL dump parser/DumpDownloader.cs
+++ Wikipedia SQL dump parser/DumpDownloader.cs
@@ -10,6 +10,7 @@ namespace WpSqlDumpParser
 		public static Stream DownloadDump(string wiki, string dump, DateTime date)
 		{
 			string url = string.Format("http://download.wikimedia.org/{0}/{2}/{0}-{2}-{1}.sql.gz", wiki, dump, date.ToString("yyyyMMdd"));
+			Console.Error.WriteLine("{0:dd.MM.yyyy hh:mm:ss} Downloading {1}.", DateTime.Now, url);
 
 			WebClient wc = new WebClient();
 			wc.Headers[HttpRequestHeader.UserAgent] = "[[w:en:User:Svick]] SQL dump parser";
