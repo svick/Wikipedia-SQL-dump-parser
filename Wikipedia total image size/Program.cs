6705167e92dc451c884c5335650cfcae22b93dc5
Petr Onderka <gsvick@gmail.com>
2010-10-07 22:33:33 +0200

    added skeleton for total image size

---
+++ Wikipedia total image size/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace WpTotalImageSize
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			Console.Write("Wiki [{0}]: ", Settings.Default.Wiki);
+			string wiki = Console.ReadLine();
+			if (string.IsNullOrWhiteSpace(wiki))
+				wiki = Settings.Default.Wiki;
+			Settings.Default.Wiki = wiki;
+			Console.Write("Date [{0}]: ", Settings.Default.Date);
+			string dateString = Console.ReadLine();
+			if (string.IsNullOrWhiteSpace(dateString))
+				dateString = Settings.Default.Date;
+			if (string.IsNullOrWhiteSpace(dateString))
+				dateString = DateTime.Today.ToString("yyyyMMdd");
+			Settings.Default.Date = dateString;
+			Console.Write("Commons date [{0}]: ", Settings.Default.CommonsDate);
+			string commonsDateString = Console.ReadLine();
+			if (string.IsNullOrWhiteSpace(commonsDateString))
+				commonsDateString = Settings.Default.CommonsDate;
+			if (string.IsNullOrWhiteSpace(commonsDateString))
+				commonsDateString = DateTime.Today.ToString("yyyyMMdd");
+			Settings.Default.CommonsDate = commonsDateString;
+			Settings.Default.Save();
+
+			DateTime date = DateTime.ParseExact(dateString, "yyyyMMdd", System.Globalization.CultureInfo.InvariantCulture);
+			DateTime commonsDate = DateTime.ParseExact(commonsDateString, "yyyyMMdd", System.Globalization.CultureInfo.InvariantCulture);
+
+		}
+	}
+}
---
+++ Wikipedia total image size/Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following 
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("Wikipedia total image size")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Wikipedia total image size")]
+[assembly: AssemblyCopyright("Copyright ©  2010")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible 
+// to COM components.  If you need to access a type in this assembly from 
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("e39d4e1c-ae2f-41a5-a299-e014b7408730")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version 
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Build and Revision Numbers 
+// by using the '*' as shown below:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
