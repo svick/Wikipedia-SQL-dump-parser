67aafae62304c2801b4ac242020368b5490ac23d
Petr Onderka <gsvick@gmail.com>
2010-10-08 00:50:17 +0200

    added ImageLink

---
+++ Wikipedia SQL dump parser/ImageLink.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace WpSqlDumpParser
+{
+	public class ImageLink
+	{
+		public int FromId { get; protected set; }
+		public string ToTitle { get; protected set; }
+
+		public Page From
+		{
+			get
+			{
+				var repo = Repository<Page>.Instance;
+				if (repo == null)
+					throw new InvalidOperationException();
+				return repo.FindById(FromId);
+			}
+		}
+
+		public ImageLink(int fromId, string toTitle)
+		{
+			FromId = fromId;
+			ToTitle = toTitle;
+		}
+	}
+}
---
+++ Wikipedia SQL dump parser/ImageLinks.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+
+namespace WpSqlDumpParser
+{
+	public class ImageLinks : Dump<ImageLink>
+	{
+		static ImageLinks instance;
+		public static ImageLinks Instance
+		{
+			get
+			{
+				if (instance == null)
+					instance = new ImageLinks();
+				return instance;
+			}
+		}
+
+		private ImageLinks()
+		{ }
+
+		public override string Name
+		{
+			get { return "imagelinks"; }
+		}
+
+		public override IEnumerable<ImageLink> Get(Stream stream)
+		{
+			Parser parser = new Parser();
+			var result = from values in parser.Parse(stream)
+									 select new ImageLink(
+										 values["from"].ToInt32(),
+										 values["to"].ToString()) into imageLink
+									 where imageLink.FromId != 0
+									 select imageLink;
+
+			if (Limiter != null)
+				result = Limiter(result);
+			return result;
+		}
+
+		public override IEnumerable<ImageLink> Get(string wiki, DateTime date)
+		{
+			return Get(wiki, date, true);
+		}
+
+		public IEnumerable<ImageLink> Get(string wiki, DateTime date, bool downloadPages)
+		{
+			if (Repository<Page>.Instance == null)
+				Pages.Instance.CreateRepository(wiki, date);
+
+			return base.Get(wiki, date);
+		}
+	}
+}
