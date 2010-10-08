225b6f8c652d459d16dfca09d65d3541fda77dc6
Petr Onderka <gsvick@gmail.com>
2010-10-08 23:26:08 +0200

    using Enumerable of byte[]s isn't necessary at all

---
+++ Wikipedia SQL dump parser/DownloadStream.cs
@@ -0,0 +1,141 @@
+﻿using System;
+using System.IO;
+using System.Net;
+
+namespace WpSqlDumpParser
+{
+	public class DownloadStream : Stream
+	{
+		public string Uri { get; protected set; }
+		public string UserAgent { get; set; }
+
+		public static bool Log { get; set; }
+		public static bool Verbose { get; set; }
+
+		int i = 0;
+		int position = 0;
+		WebResponse response = null;
+		Stream responseStream = null;
+
+		public DownloadStream(string uri)
+		{
+			Uri = uri;
+		}
+
+		public override bool CanRead
+		{
+			get { return true; }
+		}
+
+		public override bool CanSeek
+		{
+			get { return false; }
+		}
+
+		public override bool CanWrite
+		{
+			get { return false; }
+		}
+
+		public override void Flush()
+		{ }
+
+		public override long Length
+		{
+			get { throw new NotSupportedException(); }
+		}
+
+		public override long Position
+		{
+			get
+			{
+				throw new NotSupportedException();
+			}
+			set
+			{
+				throw new NotSupportedException();
+			}
+		}
+
+		public override int Read(byte[] buffer, int offset, int count)
+		{
+			while (true)
+			{
+				if (responseStream == null)
+				{
+					HttpWebRequest request = (HttpWebRequest)WebRequest.Create(Uri);
+					request.AddRange(position);
+					request.Timeout = 60000;
+
+					try
+					{
+						response = request.GetResponse();
+					}
+					catch (WebException ex)
+					{
+						if (Log)
+							Console.Error.Log(ex.Message);
+						continue;
+					}
+
+					responseStream = response.GetResponseStream();
+				}
+
+				try
+				{
+					int read = responseStream.Read(buffer, offset, count);
+					position += read;
+
+					if (Verbose)
+						Console.Error.Log(
+							string.Format(
+								"Just downloaded {0:f2} kB, total {1:f2} / {2:f2} MB.",
+								(float)read / 1024,
+								(float)position / 1024 / 1024,
+								(float)response.ContentLength / 1024 / 1024
+							));
+					else if (Log && ++i % 100 == 0)
+					{
+						Console.Error.Log(
+							string.Format(
+								"{0:f2} / {1:f2} MB",
+								(float)position / 1024 / 1024,
+								(float)response.ContentLength / 1024 / 1024
+								));
+					}
+
+					return read;
+				}
+				catch (TimeoutException)
+				{
+					if (Log)
+						Console.Error.Log("Timeout encoutered");
+					responseStream.Dispose();
+					responseStream = null;
+				}
+			}
+		}
+
+		public override long Seek(long offset, SeekOrigin origin)
+		{
+			throw new NotSupportedException();
+		}
+
+		public override void SetLength(long value)
+		{
+			throw new NotSupportedException();
+		}
+
+		public override void Write(byte[] buffer, int offset, int count)
+		{
+			throw new NotSupportedException();
+		}
+
+		protected override void Dispose(bool disposing)
+		{
+			base.Dispose(disposing);
+			if (responseStream != null)
+				responseStream.Dispose();
+		}
+	}
+}
--- Wikipedia SQL dump parser/DumpDownloader.cs
+++ Wikipedia SQL dump parser/DumpDownloader.cs
@@ -1,8 +1,6 @@
 ﻿using System;
-using System.Collections.Concurrent;
 using System.IO;
 using System.IO.Compression;
-using System.Threading.Tasks;
 using WpSqlDumpParser.Streams;
 
 namespace WpSqlDumpParser
@@ -14,35 +12,10 @@ namespace WpSqlDumpParser
 			string url = string.Format("http://download.wikimedia.org/{0}/{2}/{0}-{2}-{1}.sql.gz", wiki, dump, date.ToString("yyyyMMdd"));
 			Console.Error.WriteLine("{0:dd.MM.yyyy hh:mm:ss} Downloading {1}.", DateTime.Now, url);
 
-			BlockingCollection<byte[]> queue = new BlockingCollection<byte[]>(50);
-
-			Task.Factory.StartNew(() => Fill(queue, url));
-
-			Stream stream = new StreamFromBlockingCollection(queue);
+			Stream stream = new DownloadStream(url);
 
 			GZipStream gzipStream = new GZipStream(stream, CompressionMode.Decompress);
 			return gzipStream;
-		}
-
-		public static void Fill(BlockingCollection<byte[]> collection, string url)
-		{
-			try
-			{
-				BlockDownloader downloader = new BlockDownloader(url);
-				downloader.UserAgent = "[[w:en:User:Svick]] SQL dump parser";
-				BlockDownloader.Log = true;
-
-				foreach (var chunk in downloader)
-					collection.Add(chunk);
-			}
-			catch (Exception ex)
-			{
-				Console.Out.Log(ex.ToString());
-			}
-			finally
-			{
-				collection.CompleteAdding();
-			}
 		}
 	}
 }
