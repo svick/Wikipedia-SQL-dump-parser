58b1ee2fa85ff38883c887af175fd853191752e0
Petr Onderka <gsvick@gmail.com>
2011-01-15 17:29:36 +0100

    added TotalImageSizeExample

---
+++ TotalImageSizeExample/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Linq;
+using WpSqlDumpParser.EntityCollections;
+using WpSqlDumpParser.IO;
+
+namespace ChineseCategoryLinks
+{
+	class Program
+	{
+		static void Main()
+		{
+			CachingStream.CachePath = @"F:\Wikipedia dumps";
+
+			var categoryLinks = CategoryLinks.Instance.Get("zhwiki", new DateTime(2010, 11, 09));
+
+			foreach (var cl in categoryLinks)
+			{
+				Console.Write("{0}\t{1}", cl.From.FullName, cl.ToTitle);
+				Console.ReadLine();
+			}
+		}
+	}
+}
