feb49e645239318276801bd4baf9182d6cb2c4c4
Petr Onderka <gsvick@gmail.com>
2011-01-15 17:30:06 +0100

    added LangLink

---
+++ Wikipedia SQL dump parser/Entities/LangLink.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using WpSqlDumpParser.EntityCollections;
+
+namespace WpSqlDumpParser.Entities
+{
+	class LangLink
+	{
+		public int FromId { get; protected set; }
+		public string Lang { get; protected set; }
+		public string Title { get; protected set; }
+
+		public Page From
+		{
+			get
+			{
+				var repo = Repository<Page>.Instance;
+				if (repo == null)
+					throw new InvalidOperationException();
+				return repo.FindById(FromId);
+			}
+		}
+
+		public LangLink(int fromId, string lang, string title)
+		{
+			FromId = fromId;
+			Lang = lang;
+			Title = title;
+		}
+	}
+}
---
+++ Wikipedia SQL dump parser/EntityCollections/LangLinks.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using WpSqlDumpParser.Entities;
+using WpSqlDumpParser.Parsing;
+
+namespace WpSqlDumpParser.EntityCollections
+{
+	class LangLinks : Dump<LangLink>
+	{
+		static LangLinks instance;
+		public static LangLinks Instance
+		{
+			get
+			{
+				if (instance == null)
+					instance = new LangLinks();
+				return instance;
+			}
+		}
+
+		private LangLinks()
+		{ }
+
+		public override string Name
+		{
+			get { return "langlinks"; }
+		}
+
+		public override IEnumerable<LangLink> Get(Stream stream)
+		{
+			Parser parser = new Parser();
+			var result = from values in parser.Parse(stream)
+									 select new LangLink(
+										 values["from"].ToInt32(),
+										 values["lang"].ToString(),
+										 values["title"].ToString()) into categoryLink
+									 where categoryLink.FromId != 0
+									 select categoryLink;
+
+			if (Limiter != null)
+				result = Limiter(result);
+			return result;
+		}
+
+		public override IEnumerable<LangLink> Get(string wiki, DateTime date)
+		{
+			return Get(wiki, date, true);
+		}
+
+		public IEnumerable<LangLink> Get(string wiki, DateTime date, bool downloadPages)
+		{
+			if (downloadPages && Repository<Page>.Instance == null)
+				Pages.Instance.CreateRepository(wiki, date);
+
+			return base.Get(wiki, date);
+		}
+	}
+}
